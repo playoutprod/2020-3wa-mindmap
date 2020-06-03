66e8e6993ccce5d3e0ffc181ff863dc7dc24b3aa
playoutprod <35104962+playoutprod@users.noreply.github.com>
2020-06-03 20:18:14 +0200

    Supress spaces

--- src/components/NodeLink.js
+++ src/components/NodeLink.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import Add from './Add'
 import '../css/nodelink.css';
+
+
+
+
+
+
 export default (props)=>(
   <div className={"nodelink "+props.type+" "+(props.childs ? 'childs' : '')}>
     <div className="line" style={{backgroundColor:(props.color ? props.color : "#FFF")}}></div>
